d4572ad07908c94b657e3cfa1dc68a8db586fccc
TkachukSasha <sashatkachuk09@ukr.net>
2022-09-04 19:52:23 +0300

    #0016 feat: create sql project for store project scripts also refactor docker-compose override file

---
+++ HostelBooking.Backend/src/Scripts/Hostel.Scripts/Version_1_CreateTablesForSecurityApiAndCatalogueApi.sql
@@ -0,0 +1,39 @@
+﻿Create Table "User"(
+  UserId Int Identity(1,1) Primary Key,
+  Email Nvarchar(100) Not Null, 
+  Password Nvarchar(200) Not Null, 
+  Role Nvarchar(20) Not Null,
+  CreatedAt Date,
+  IsDeleted Bit
+)
+
+Create Table RefreshToken(
+  RefreshTokenId Int Identity(1,1) Primary Key,
+  Token Nvarchar(MAX) Not Null, 
+  UserId Int
+)
+
+Alter Table RefreshToken
+Add Foreign Key (UserId) References "User"(UserId)
+
+
+
+Create Table Company(
+  CompanyId Int Identity(1,1) Primary Key,
+  Name Nvarchar(100) Not Null,
+  Description Nvarchar(100) Not Null,
+  City Nvarchar(100) Not Null,
+  IsDeleted Bit
+)
+
+Create Table Room(
+  RoomId Int Identity(1,1) Primary Key,
+  Number Int Not Null,
+  Floor Int Not Null,
+  Capacity Int Not Null,
+  IsDeleted Bit,
+  CompanyId Int
+)
+
+Alter Table Room
+Add Foreign Key (CompanyId) References Company(CompanyId)
